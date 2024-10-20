110bd33deced431adac7d2648cda59ac6c0ad1ce
NaNlaggergit <hlo.lyashenko@yandex.ru>
2024-10-20 16:56:16 +0700

    Add project files.

---
+++ HeatmapTask.cs
@@ -0,0 +1,24 @@
+﻿using Avalonia;
+using System.Diagnostics.Metrics;
+
+namespace Names;
+
+internal static class HeatmapTask
+{
+    public static HeatmapData GetBirthsPerDateHeatmap(NameData[] names)
+    { 
+        string[] month = new[] { "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12" };
+        string[] data = new[] { "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31" };
+        double[,] intensityPoints = new double[30, 12];
+        for (int i = 0; i < names.Length; i++)
+        {
+            if(names[i].BirthDate.Day!=1)
+                intensityPoints[names[i].BirthDate.Day -2, names[i].BirthDate.Month - 1]++;
+        }
+        return new HeatmapData(
+            "Пример карты интенсивностей",
+            intensityPoints, 
+            data, 
+            month);
+    }
+}
---
+++ HistogramTask.cs
@@ -0,0 +1,23 @@
+﻿namespace Names;
+
+internal static class HistogramTask
+{
+    public static HistogramData GetBirthsPerDayHistogram(NameData[] names, string name)
+    {
+        string[] data = new[] { "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31" };
+        double[] countName = new double[31];
+        for (int i= 0, day=0; i < names.Length; i++)
+        {
+            if(names[i].Name == name)
+            {
+                day=names[i].BirthDate.Day;
+                countName[day-1]++;
+            }
+        }
+        countName[0] = 0d;
+        return new HistogramData(
+            $"Рождаемость людей с именем '{name}'", 
+            data,
+            countName);
+    }
+}
